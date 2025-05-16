39d64ce1587bfbdf9c26e972eed40b2385ee025e
bhushan <patilb847@gmail.com>
2025-05-16 22:45:06 +0530

    initial commit : powershell cleanup code

---
+++ cleanup/cleanup.ps1
@@ -0,0 +1,15 @@
+﻿$target_dir="C:\Users\1000071390\Downloads"
+$Today=Get-Date
+$log_file="C:\D-drive\powershell\cleanup\cleanup_log"+($Today.Day)+($Today.Month)+($Today.Year)+".txt"
+$log_file
+$check_date=(Get-Date).AddDays(-30)
+$files=(Get-ChildItem -Path $target_dir -File| where{ 
+
+$_.LastWriteTime -gt $check_date 
+
+})
+
+foreach($file in $files){
+    #Remove-Item -Path $file.FullName -Force
+    $file.FullName +" Deleted on "+$Today | Out-File $log_file -Append
+}
