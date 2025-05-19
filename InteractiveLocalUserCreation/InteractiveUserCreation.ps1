48d258d20cb2f9a864f5d4db0ad116d31a5a8d4d
patilb847 <patilb847@gmail.com>
2025-05-19 15:37:25 +0000

    Added Interactive local user creation script

--- InteractiveLocalUserCreation/InteractiveUserCreation.ps1
+++ InteractiveLocalUserCreation/InteractiveUserCreation.ps1
@@ -8,62 +8,66 @@ while($continue_flag -eq $true){
     $first_name=Read-Host -Prompt "Enter first name of user"
 
     while(($first_name -eq $null) -or ($first_name -eq "")){
-        "Firstname is mandetory"
+        "Firstname is mandatory"
         $first_name=Read-Host -Prompt "Enter first name of user"
     }
 
 
     $last_name=Read-Host -Prompt "Enter last name of user"
     while(($last_name -eq $null) -or ($last_name -eq "")){
-        "Lastname is mandetory"
+        "Lastname is mandatory"
         $last_name=Read-Host -Prompt "Enter lastname of user"
     }
 
     $user_name=Read-Host -Prompt "Enter username of user"
     while(($user_name -eq $null) -or ($user_name -eq "")){
-        "Username is mandetory"
+        "Username is mandatory"
         $user_name=Read-Host -Prompt "Enter username of user"
     }
 
 
-    $password=Read-Host -Prompt "Enter password for user" -AsSecureString 
+    $password=Read-Host -Prompt "Enter password for user" 
     while(($password -eq $null) -or ($password -eq "") -or ($password.Length -lt 8)){
-        "Password is mandetory, Password should be more than 8 characters"
-        $password=Read-Host -Prompt "Enter password for user" -AsSecureString
+        "Password is mandatory, Password should be more than 8 characters"
+        $password=Read-Host -Prompt "Enter password for user"
     }
-
+    $password=ConvertTo-SecureString -String $password -AsPlainText -Force
 
 
     try{
         $existing_user=get-localuser -Name $user_name -ErrorAction Stop
         "==========================================================================="
-        Write-Host "[$((Get-Date -Format dd-MM-yyyy)) $((Get-Date -Format HH:mm:ss))] User $($existing_user) already exists " | Out-File $log_file -Append
+        "[$((Get-Date -Format dd-MM-yyyy)) $((Get-Date -Format HH:mm:ss))] User $($existing_user) already exists " | Out-File $log_file -Append
+        "[$((Get-Date -Format dd-MM-yyyy)) $((Get-Date -Format HH:mm:ss))] User $($existing_user) already exists "
         "==========================================================================="
     }catch{
         if($_.exception.message -eq "User $user_name was not found."){
             try{
                 $new_user=New-LocalUser -Name $user_name -Password ($password) -FullName "$first_name $last_name" -ErrorAction Stop
                 "==========================================================================="
-                Write-Host "[ $((Get-Date -Format dd-MM-yyyy)) $((Get-Date -Format HH:mm:ss)) ] successfully created user $($user_name)" | Out-File $log_file -Append
+                "[ $((Get-Date -Format dd-MM-yyyy)) $((Get-Date -Format HH:mm:ss)) ] successfully created user $($user_name)" | Out-File $log_file -Append
+                "[ $((Get-Date -Format dd-MM-yyyy)) $((Get-Date -Format HH:mm:ss)) ] successfully created user $($user_name)"
                 "==========================================================================="
             }catch{
                 "==========================================================================="
-                Write-Host "[ $((Get-Date -Format dd-MM-yyyy)) $((Get-Date -Format HH:mm:ss))] failed to create user $($user_name), following error occured $($_.exception.message)" | Out-File $log_file -Append
+                "[ $((Get-Date -Format dd-MM-yyyy)) $((Get-Date -Format HH:mm:ss))] failed to create user $($user_name), following error occured $($_.exception.message)" | Out-File $log_file -Append
+                "[ $((Get-Date -Format dd-MM-yyyy)) $((Get-Date -Format HH:mm:ss))] failed to create user $($user_name), following error occured $($_.exception.message)"
                 "==========================================================================="
             }
         }else{
             "==========================================================================="
-            Write-Host "[$((Get-Date -Format dd-MM-yyyy)) $((Get-Date -Format HH:mm:ss))] failed to create user $($user_name), following error occured $($_.exception.message)" | Out-File $log_file -Append
+            "[$((Get-Date -Format dd-MM-yyyy)) $((Get-Date -Format HH:mm:ss))] failed to create user $($user_name), following error occured $($_.exception.message)" | Out-File $log_file -Append
+            "[$((Get-Date -Format dd-MM-yyyy)) $((Get-Date -Format HH:mm:ss))] failed to create user $($user_name), following error occured $($_.exception.message)"
             "==========================================================================="
         }
     }
-    $continure_dec=""
-    while(($continure_dec -ne "y") -and ($continure_dec -ne "n")){
-        $continure_dec=Read-Host -Prompt "Do you want to create another user(y/n)"
+    $continue_dec=""
+    while(($continue_dec -ne "y") -and ($continue_dec -ne "n")){
+        $continue_dec=Read-Host -Prompt "Do you want to create another user(y/n)"
     
-        if($continure_dec -eq "y"){
+        if($continue_dec -eq "y"){
             $continue_flag=$true
-        }elseif($continure_dec -eq "n"){
+        }elseif($continue_dec -eq "n"){
             $continue_flag=$false
         }
     }
